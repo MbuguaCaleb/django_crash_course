de2e82cae4a22d74e9d55dbe62d79c10dc242fce
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-02-25 19:48:35 +0300

    adding-posts

--- pollstar/polls/views.py
+++ pollstar/polls/views.py
@@ -2,7 +2,9 @@
 # Create your views here.
 # Render templates or  return JSON
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from .models import Question, Choice
 
 
@@ -27,5 +29,26 @@ def detail(request, question_id):
 
 # Get question and display results
 def results(request, question_id):
-    question = Question.objects.get(pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+
+# Vote for Question
+def vote(request, question_id):
+    # print(request.POST['choice'])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
